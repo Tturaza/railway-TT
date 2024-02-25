e558658ee031fff00cad8a107c970bca2f8eed38
Tturaza <temoturazashvili9@gmail.com>
2024-02-25 18:59:33 +0400

    new validation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { FormBuilder, Validators} from '@angular/forms';
 import { MatDatepickerControl, MatDatepickerPanel } from '@angular/material/datepicker';
 import { Router } from '@angular/router';
@@ -8,18 +8,15 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnChanges{
+export class HomeComponent implements OnInit{
 
   minDate  = new Date()
   picker!: MatDatepickerPanel<MatDatepickerControl<any>,any,any>;
 
   constructor(private fb :FormBuilder , private router : Router){}
 
-  ngOnChanges(changes: SimpleChanges): void {
-   
-  }
-
   ngOnInit(): void {}
+
   myInfo: any;
   weekday: string | undefined;
   myDay:any;
@@ -67,16 +64,16 @@ export class HomeComponent implements OnChanges{
       this.router.navigate(['/trains']);
     }
 
-  
+    validationToFrom(){
+      const fromValue  = this.directionObj.get("from")?.value
+      const toValue = this.directionObj.get("to")?.value
 
+      if(fromValue === toValue){
+        this.directionObj.get("to")?.setErrors({'sameAsFrom' :true})
+        window.alert("In this case, the train cannot be found.You must Change the 'To' values")
+        
+      }else{
+        this.directionObj.get("to")?.setErrors(null)
+      }
+    }
 }
-
-//with swagger
-
-//CgetMethod(){
-  //   this.serv.SgetMethod().subscribe( respo => {
-  //     this.myInfo = respo
-  //     console.log(respo)
-  //   } ) 
- 
-  //   }
