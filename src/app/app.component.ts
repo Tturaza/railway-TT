c403d75476614ffeb639e6de7e47828c6950052c
Tturaza <temoturazashvili9@gmail.com>
2024-03-06 21:31:47 +0400

    project finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from './firebase.config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+ 
+ 
+  ngOnInit(): void {
+   initializeApp(firebaseConfig)
+  }
+
   title = 'startProject';
 
 
---
+++ src/app/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBrHqHG_XTN9pbbs8-AhdBY5T-H2jfAkpI",
+    authDomain: "trainwaytt.firebaseapp.com",
+    projectId: "trainwaytt",
+    storageBucket: "trainwaytt.appspot.com",
+    messagingSenderId: "915353586773",
+    appId: "1:915353586773:web:55faca14a4b549b6c3d8dc"
+  };
+  
